243c05568f9c6be9e5f2a20a364b1774e62e0394
Demmy123 <1055759@hr.nl>
2023-06-15 10:27:46 +0200

    Even mapjes gemaakt

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,16 +2,19 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import beeImage from "../images/bee2.png"
 import spiderImage from "../images/spoeder.png"
 import buttonImage from "../images/button.png"
+import flowerImage from "../images/flower.png"
 
 const Resources = {
     Bee: new ImageSource(beeImage),
     Spider: new ImageSource(spiderImage),
-    Button: new ImageSource(buttonImage)
+    Button: new ImageSource(buttonImage),
+    Flower: new ImageSource(flowerImage)
 }
 const ResourceLoader = new Loader([
     Resources.Bee,
     Resources.Spider,
-    Resources.Button
+    Resources.Button,
+    Resources.Flower
 ])
 
 export { Resources, ResourceLoader }
