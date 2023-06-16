d600ec325d482f04bf0df1047a73373a7b13d89a
Flurry19 <sharonirenedewit@gmail.com>
2023-06-16 10:44:41 +0200

    Borders and background fix

--- src/js/Scenes/1Level.js
+++ src/js/Scenes/1Level.js
@@ -10,13 +10,38 @@ export class LevelOne extends ex.Scene {
     onInitialize(Engine) {
 
         const backgroundImage = Resources.Background.toSprite();
-        const background = new Background(0, 0, 200, 20, backgroundImage);
+        const background = new Background(-500, -100, 200, 20, backgroundImage);
         this.add(background);
 
         // const platform = new Platform();
         // this.add(platform);
         // platform.pos = new ex.Vector(140, 550);
 
+        // Right invisible wall
+        let leftWall = new ex.Actor({
+            pos: ex.vec(-1, 0),
+            width: 30,
+            height: 9000000000,
+            collisionType: ex.CollisionType.Fixed
+        })
+        // Left invisible wall
+        let rightWall = new ex.Actor({
+            pos: ex.vec(9500, 0),
+            width: 30,
+            height: 9000000000,
+            collisionType: ex.CollisionType.Fixed
+        })
+        // Roof
+        let foundation = new ex.Actor({
+            pos: ex.vec(0, 600),
+            width: 10000,
+            height: 10,
+            collisionType: ex.CollisionType.Fixed
+        })
+
+        this.add(foundation)
+        this.add(leftWall);
+        this.add(rightWall)
         // Platform on the bottom
         const platform1 = new Platform(0, 580, 10000, 20, ex.Color.Green);
         platform1.scale = new ex.Vector(10.9, 1)
