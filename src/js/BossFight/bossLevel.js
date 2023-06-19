f785f86853b5dbbac2b75974037883bbfab88072
PurpleAliennn <elinekers@gmail.com>
2023-06-19 13:48:41 +0200

    fixed the damn git merge fuckup

---
+++ src/js/BossFight/bossLevel.js
@@ -0,0 +1,25 @@
+import { Vector, Physics, Scene } from "excalibur";
+
+import { BossSpider } from "./boss";
+import { Maincharacter } from "../Actors/character";
+
+export class BossFight extends Scene {
+
+    constructor(){
+
+        super({
+            width: 1920,
+            height: 1080
+        })
+
+        Physics.useRealisticPhysics();
+        Physics.gravity = new Vector(0, 800);
+
+    }
+
+    onInitialize(){
+
+        const boss = new BossSpider();
+        this.add(boss);
+    }
+}
