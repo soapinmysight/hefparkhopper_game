10a0b2f358f63f9469ad14909e581da8678af401
Demmy123 <1055759@hr.nl>
2023-06-21 13:38:49 +0200

    code update voor meerdere levels

--- src/js/Actors/platform.js
+++ src/js/Actors/platform.js
@@ -1,7 +1,8 @@
 import * as ex from 'excalibur'
 import { Resources, ResourceLoader } from '../resources';
 
-export class Platform extends ex.Actor {
+
+export class PlatformLvlOne extends ex.Actor {
     constructor(x, y, width, height) {
         super({
             pos: new ex.Vector(x, y),
@@ -14,7 +15,18 @@ export class Platform extends ex.Actor {
         this.graphics.use(Resources.Platform.toSprite()); //Image of platform
         this.scale = new ex.Vector(0.3, 0.3)
     }
-
-
-
+}
+export class PlatformLvlTwo extends ex.Actor {
+    constructor(x, y, width, height) {
+        super({
+            pos: new ex.Vector(x, y),
+            width: width,
+            height: height,
+            anchor: ex.Vector.Zero, //Anchors the platforms
+            collisionType: ex.CollisionType.Fixed, //Bee bounces against the platforms
+            collisionGroup: ex.CollisionGroupManager.groupByName('platform'),//Make sure you bounce against all the platforms
+        });
+        this.graphics.use(Resources.Platform.toSprite()); //Image of platform
+        this.scale = new ex.Vector(0.3, 0.3)
+    }
 }
--- src/js/Actors/spikes.js
+++ src/js/Actors/spikes.js
@@ -2,7 +2,22 @@ import * as ex from 'excalibur'
 import { Resources, ResourceLoader } from '../resources';
 import {Maincharacter} from "./character.js";
 
-export class Spikes extends ex.Actor {
+export class MainSpike extends ex.Actor {
+
+    onInitialize(engine) {
+        super.onInitialize(engine);
+        this.body.collisionType = ex.CollisionType.Fixed;
+        this.on("precollision", (event) => this.onPreCollision(event));
+    }
+    onPreCollision(event) {
+        const otherActor = event.other
+        if(otherActor instanceof Maincharacter){
+            otherActor.kill();
+        }
+    }
+}
+
+export class SpikesLvlOne extends MainSpike {
     constructor(x, y, width, height) {
         super({
             pos: new ex.Vector(x, y),
@@ -15,20 +30,32 @@ export class Spikes extends ex.Actor {
         this.graphics.use(Resources.Spikes.toSprite()); //Image of platform
         this.scale = new ex.Vector(0.5, 0.5)
     }
-    onInitialize(engine) {
-        super.onInitialize(engine);
-        this.body.collisionType = ex.CollisionType.Active;
-        this.on("precollision", (event) => this.onPreCollision(event));
+}
+export class SpikesLvlTwo extends MainSpike {
+    constructor(x, y, width, height) {
+        super({
+            pos: new ex.Vector(x, y),
+            width: width,
+            height: height,
+            anchor: ex.Vector.Zero, //Anchors the platforms
+            // collisionType: ex.CollisionType.Fixed, //Bee bounces against the platforms
+            // collisionGroup: ex.CollisionGroupManager.groupByName('spikes'),//Make sure you bounce against all the platforms
+        });
+        this.graphics.use(Resources.Spikes.toSprite()); //Image of platform
+        this.scale = new ex.Vector(0.5, 0.5)
     }
-    onPreCollision(event) {
-        const otherActor = event.other
-
-        if(otherActor instanceof Maincharacter){
-            otherActor.kill();
-        }
-
+}
+export class SpikesLvlThree extends MainSpike {
+    constructor(x, y, width, height) {
+        super({
+            pos: new ex.Vector(x, y),
+            width: width,
+            height: height,
+            anchor: ex.Vector.Zero, //Anchors the platforms
+            // collisionType: ex.CollisionType.Fixed, //Bee bounces against the platforms
+            // collisionGroup: ex.CollisionGroupManager.groupByName('spikes'),//Make sure you bounce against all the platforms
+        });
+        this.graphics.use(Resources.Spikes.toSprite()); //Image of platform
+        this.scale = new ex.Vector(0.5, 0.5)
     }
-
-
-
 }
