64035afcbf9d314cc72133223f7e3898c86582b0
Flurry19 <sharonirenedewit@gmail.com>
2023-06-26 11:21:55 +0200

    spike platform

--- src/js/Actors/flower.js
+++ src/js/Actors/flower.js
@@ -10,7 +10,7 @@ export class Flower extends ex.Actor {
             width: 2250,
             height: 2130,
             anchor: ex.Vector.Zero, //Anchors the flowers
-            collisionType: ex.CollisionType.Fixed, //Bee bounces against the flowers
+            collisionType: ex.CollisionType.Passive, //Bee bounces against the flowers
             collisionGroup: ex.CollisionGroupManager.groupByName('flower'),//Make sure you bounce against all the platforms
         });
         this.graphics.use(Resources.Flower.toSprite()); //Image of flower
