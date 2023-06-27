920cd67a88460ee7de40b6523e36e0c6a96d0bd5
Demmy123 <1055759@hr.nl>
2023-06-27 12:33:06 +0200

    resoures toegevoegd

---
+++ src/js/cutScene/actors/background.js
@@ -0,0 +1,46 @@
+import * as ex from 'excalibur';
+import {Resources} from "../resources.js";
+
+// export class BaseBackground extends ex.Actor {
+//     constructor(x, y, width, height, image) {
+//         super({
+//             pos: new ex.Vector(x, y),
+//             width: width,
+//             height: height,
+//             anchor: ex.Vector.Zero,
+//         });
+//
+//         const sprite = new ex.Sprite(image);
+//         this.graphics.use(sprite);
+//     }
+// }
+//
+// export class BackgroundLvlTwo extends ex.Actor {
+//     constructor(x, y) {
+//         super({
+//             pos: new ex.Vector(x, y),
+//             height: 600,
+//             width: 10000,
+//             anchor: ex.Vector.Zero,
+//         });
+//         const sprite = Resources.BackgroundLvlTwo.toSprite();
+//         sprite.height = 600
+//         sprite.width = 10000
+//         this.graphics.add(sprite);
+//     }
+// }
+//
+// export class BackgroundLvlThree extends ex.Actor {
+//     constructor(x, y) {
+//         super({
+//             pos: new ex.Vector(x, y),
+//             height: 600,
+//             width: 9101,
+//             anchor: ex.Vector.Zero,
+//         });
+//         const sprite = Resources.BackgroundLvlThree.toSprite();
+//         sprite.height = 600
+//         sprite.width = 9101
+//         this.graphics.add(sprite);
+//     }
+// }
